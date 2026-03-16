ab60f8db09059b11fb01701dca764df2d078791b
MUGABE GIDEON <mugabegideon44@gmail.com>
2026-03-16 22:06:17 +0300

    feat: add Django REST API for crop prices

--- backend/prices/views.py
+++ backend/prices/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+@api_view(['GET'])
+def get_prices(request):
+    prices = [
+        {"id": 1, "crop": "Maize", "district":"Kampala", "price": 1200},
+        {"id": 2, "crop": "Beans", "district":"Gulu", "price": 3500},
+        {"id": 3, "crop": "Cassava", "district":"Mbale", "price": 800},
+        {"id": 4, "crop": "Rice", "district":"Lira", "price": 2800},
+        ]
+    return Response(prices)
